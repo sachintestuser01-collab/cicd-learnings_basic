32796c42b5ed49d2f1e5a4e4b5ed58ec3b0fd7b2
unknown <sachintestuser01@gmail.com>
2025-08-27 11:31:39 +0530

    added more test and functions in src and test folder

--- tests/test_main_func.py
+++ tests/test_main_func.py
@@ -1,6 +1,22 @@
-from src.main import add
+from src.main import add, sub, mul, div
 
 def test_add():
     assert add(1, 2) == 3
     assert add(3, 2) == 5
     assert add(5, 5) == 10
+
+def test_sub():
+    assert sub(5, 2) == 3
+    assert sub(4, 2) == 2
+    assert sub(4, 3) == 1
+
+def test_mul():
+    assert mul(5, 2) == 10
+    assert mul(4, 2) == 8
+    assert mul(4, 3) == 12
+
+
+def test_div():
+    assert div(10, 2) == 5
+    assert div(4, 2) == 2
+    assert div(9, 3) == 3
